1e4ad73688bdeafe0c4861b027931b56b5de197e
Pedro Henrique <pedro.pereira@visto.global>
2023-07-04 02:17:50 -0300

    Iniciando Header responsivo

---
+++ eong-sistema-front/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.scss'
+export default function Header() {
+    return (
+        <nav className="header">
+            <picture className="header-logo"> <img src="/src/assets/images/jalemcancer.svg" alt="Logo da entidade" /></picture>
+            <ul className='header-nav'>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Doar</li>
+                <li>Contato</li>
+                <i className="bi bi-person-circle"></i>
+            </ul>
+        </nav>
+    )
+}
---
+++ eong-sistema-front/src/pages/public/index/Index.tsx
@@ -0,0 +1,16 @@
+import Header from '~/components/Header/Header';
+import logo from '~/logo.svg';
+
+export default function Index() {
+  return (
+    <main>
+      <Header />
+      <section></section>
+      <section>
+        <article>Nossa missão</article>
+        <article>Valores</article>
+        <article>Seja um apoiador</article>
+      </section>
+    </main>
+  )
+}
